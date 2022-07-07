635bd7d1d75ee55175081ed7d773a4b02ae090ad
Lea Hergert <lhergert@cursorinsight.com>
2022-07-07 11:45:13 +0200

    feat: Implement basic functionality

--- src/Patterns.jl
+++ src/Patterns.jl
@@ -6,4 +6,110 @@
 
 module Patterns
 
+###=============================================================================
+### Exports
+###=============================================================================
+
+export @pattern, @with_pattern
+
+###=============================================================================
+### Imports
+###=============================================================================
+
+include("Utilities.jl")
+
+using MacroTools: @capture, postwalk
+using .Utilities: patterns, show_pattern_type, symbol_to_val
+
+###=============================================================================
+### Implementation
+###=============================================================================
+
+"""
+    @with_pattern empty function declaration
+
+Create a function which allows the usage of symbols as a pattern in function
+signatures. Must be called on an empty function declaration.
+
+# Examples
+```jldoctest
+julia> @with_pattern function f end
+f (generic function with 1 method)
+```
+"""
+macro with_pattern(f)
+    @assert f.head == :function
+    @assert(length(f.args) == 1,
+        "The macro must be called on an empty function declaration!")
+    @assert f.args[1] isa Symbol "The function must be named!"
+
+    f_name = esc(f.args[1])
+
+    return quote
+        $(esc(f))
+        function $f_name(args...)
+            _args = symbol_to_val.(args)
+            called_signature =
+                Base.signature_type($f_name, Base.to_tuple_type(typeof(_args)))
+            try
+                if !any(signature -> called_signature <: signature,
+                        patterns($f_name))
+                    throw(MethodError($f_name, _args))
+                end
+
+                return $f_name(_args...)
+            catch e
+                if e isa MethodError && e.f == $f_name
+                    f_name = $f_name
+                    params = typeof(_args).parameters
+                    str = join(params .|> show_pattern_type, ", ")
+                    @error "Missing pattern $f_name($str)"
+                end
+                rethrow(e)
+            end
+        end
+    end
+end
+
+"""
+    @pattern function definition
+
+Create a function with symbols as pattern in the signature.
+
+# Examples
+```jldoctest
+julia> @pattern function f(:one)
+           return 1
+       end
+f (generic function with 2 methods)
+
+julia> @pattern f(:two) = 2
+f (generic function with 3 methods)
+
+julia> @pattern function f(:num, x::Number)::Number
+           return x
+       end
+f (generic function with 4 methods)
+```
+"""
+macro pattern(f)
+    is_function = false
+
+    expr = postwalk(f) do x
+        is_captured = @capture(x, f_name_(args__))
+        if is_captured
+            is_function = true
+            new_args::Vector = map(args) do arg
+                return arg isa QuoteNode ? :(::Val{$(esc(arg))}) : arg
+            end
+            return :($(esc(f_name))($(new_args...)))
+        end
+        return x
+    end
+
+    @assert is_function "Expected a function!"
+
+    return expr
+end
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -10,10 +10,60 @@
 
 using Test
 
-using Patterns
+using Patterns: @pattern,  @with_pattern
 
 ###=============================================================================
 ### Tests
 ###=============================================================================
 
-@test Patterns isa Module
+@testset "Pattern" begin
+    @with_pattern function f end
+
+    @test f isa Function
+    @test length(methods(f)) == 1 # there is a default wrapper
+    @test hasmethod(f, Tuple{Vararg{Symbol}})
+
+    @pattern function f(:x)
+        return "x"
+    end
+
+    @test length(methods(f)) == 2
+    @test hasmethod(f, Tuple{Val{:x}})
+    @test f(:x) isa String
+    @test f(:x) == "x"
+
+    @pattern function f(:y)::Char
+        return 'y'
+    end
+
+    @test length(methods(f)) == 3
+    @test hasmethod(f, Tuple{Val{:y}})
+    @test f(:y) isa Char
+    @test f(:y) == 'y'
+
+    @pattern function f(:z, num::Integer)
+        return num
+    end
+
+    @test length(methods(f)) == 4
+    @test hasmethod(f, Tuple{Val{:y}, Integer})
+    @test f(:z, 42) == 42
+
+    @pattern function f(:a, :b, :c)
+        return "abc"
+    end
+
+    @test length(methods(f)) == 5
+    @test hasmethod(f, Tuple{Val{:a}, Val{:b}, Val{:c}})
+    @test f(:a, :b, :c) == "abc"
+
+    @pattern function f(:d, str::T) where {T <: AbstractString}
+        return str
+    end
+
+    @test length(methods(f)) == 6
+    @test hasmethod(f, Tuple{Val{:d}, AbstractString})
+    @test f(:d, "something") == "something"
+
+    @test_throws AssertionError @macroexpand @pattern a = 1
+end
