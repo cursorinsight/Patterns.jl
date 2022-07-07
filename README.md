34dedea2c5823d6063eed76050803930d46976c2
Lea Hergert <lhergert@cursorinsight.com>
2022-07-07 11:45:13 +0200

    feat: Implement basic functionality

---
+++ README.md
@@ -0,0 +1,76 @@
+# Patterns.jl
+
+Patterns.jl is a Julia package that provides support for pattern matching.
+
+## Installation
+
+```julia
+julia>]
+pkg> add https://github.com/cursorinsight/Patterns.jl
+```
+
+## Usage
+
+Load the package via
+
+```julia
+using Patterns
+```
+
+This exports two macros, `@with_pattern` and `@pattern`.
+
+To use symbols as a pattern in function signatures first create an empty
+function with the usage of `@with_pattern`:
+
+```julia
+@with_pattern function f end
+```
+
+Then you can create functions with pattern in the signature using `@pattern`:
+
+```julia
+@pattern function f(:one)
+    return 1
+end
+```
+
+You can also use multiple symbols and other parameters in the signature:
+
+```julia
+@pattern function f(:one, :string)
+    return "1"
+end
+
+@pattern function f(x::Number, :number)
+    return x
+end
+```
+
+
+## Advanced example
+
+Create fixtures that provide data for your tests.
+
+```julia
+@with_pattern function fixture end
+
+@pattern function fixture(:numbers)
+    return [1, 7, 12]
+end
+
+@pattern function fixture(:numbers, x)
+    return [1, 7, 12, x]
+end
+
+@pattern function fixture(:numbers, :even)
+    return [2, 8, 12]
+end
+```
+
+## Notes
+
+### Limitations
+
+Be aware that keyword arguments are currently not working when you are using
+symbols as a pattern in the function signature. This will be added in a future
+update.
--- src/Patterns.jl
+++ src/Patterns.jl
@@ -6,4 +6,105 @@
 
 module Patterns
 
+###=============================================================================
+### Exports
+###=============================================================================
+
+export @pattern, @with_pattern
+
+###=============================================================================
+### Imports
+###=============================================================================
+
+include("Utilities.jl")
+
+using MacroTools: @capture, combinedef, splitdef
+using .Utilities: patterns, show_pattern_type, symbol_to_val
+
+###=============================================================================
+### Implementation
+###=============================================================================
+
+"""
+    @with_pattern empty function declaration
+
+Create a function which allows the usage of symbols as a pattern in function
+signatures. Must be called on an empty function declaration.
+
+# Examples
+```jldoctest
+julia> @with_pattern function f end
+f (generic function with 1 method)
+```
+"""
+macro with_pattern(f)
+    is_captured = @capture(f, function f_name_ end)
+
+    @assert is_captured
+        "The macro must be called on an empty function declaration!"
+
+    f_name = esc(f_name)
+
+    return quote
+        $(esc(f))
+        function $f_name(args...)
+            _args = symbol_to_val.(args)
+            called_signature =
+                Base.signature_type($f_name, Base.to_tuple_type(typeof(_args)))
+            try
+                if !any(signature -> called_signature <: signature,
+                        patterns($f_name))
+                    throw(MethodError($f_name, _args))
+                end
+
+                return $f_name(_args...)
+            catch e
+                if e isa MethodError && e.f == $f_name
+                    f_name = $f_name
+                    params = typeof(_args).parameters
+                    str = join(params .|> show_pattern_type, ", ")
+                    @error "Missing pattern $f_name($str)"
+                end
+                rethrow(e)
+            end
+        end
+    end
+end
+
+"""
+    @pattern function definition
+
+Create a function with symbols as pattern in the signature.
+
+# Examples
+```jldoctest
+julia> @pattern function f(:one)
+           return 1
+       end
+f (generic function with 2 methods)
+
+julia> @pattern f(:two) = 2
+f (generic function with 3 methods)
+
+julia> @pattern function f(:double, x::Number)::Number
+           return 2x
+       end
+f (generic function with 4 methods)
+
+julia> f(:two)
+2
+
+julia> f(:double, 123)
+246
+```
+"""
+macro pattern(f)
+    def::Dict = splitdef(f)
+    def[:args] = map(do_replace, def[:args])
+    return esc(combinedef(def))
+end
+
+do_replace(arg::QuoteNode) = :(::Val{$arg})
+do_replace(arg) = arg
+
 end # module
