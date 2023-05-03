65044a88fe221b367f554a0bacab2704784d69b7
Oliver Mate Torteli <oliver@cursorinsight.com>
2023-05-03 16:13:25 +0200

    refactor: Simplify the concept
    - Rework `@pattern` macro to handle both function definition and
      function calling as well.
    - Remove `@with_pattern` macro, so you can create a pattern for any
      existing function also.
    - Remove `@testset` macro from `test/runtests.jl`, because of its code
      alternating behaviour.

--- src/Patterns.jl
+++ src/Patterns.jl
@@ -10,71 +10,23 @@ module Patterns
 ### Exports
 ###=============================================================================
 
-export @pattern, @with_pattern
+export @pattern, @p
 
 ###=============================================================================
 ### Imports
 ###=============================================================================
 
-include("Utilities.jl")
-
-using MacroTools: @capture, combinedef, splitdef
-using .Utilities: patterns, show_pattern_type, symbol_to_val
+using MacroTools: postwalk, isshortdef, combinedef, splitdef
+using Base: Fix1
 
 ###=============================================================================
 ### Implementation
 ###=============================================================================
 
 """
-    @with_pattern empty function declaration
+    @pattern <function definition>
 
-Create a function which allows the usage of symbols as a pattern in function
-signatures. Must be called on an empty function declaration.
-
-# Examples
-```jldoctest
-julia> @with_pattern function f end
-f (generic function with 1 method)
-```
-"""
-macro with_pattern(f)
-    is_captured = @capture(f, function f_name_ end)
-
-    @assert is_captured
-        "The macro must be called on an empty function declaration!"
-
-    f_name = esc(f_name)
-
-    return quote
-        $(esc(f))
-        function $f_name(args...)
-            _args = symbol_to_val.(args)
-            called_signature =
-                Base.signature_type($f_name, Base.to_tuple_type(typeof(_args)))
-            try
-                if !any(signature -> called_signature <: signature,
-                        patterns($f_name))
-                    throw(MethodError($f_name, _args))
-                end
-
-                return $f_name(_args...)
-            catch e
-                if e isa MethodError && e.f == $f_name
-                    f_name = $f_name
-                    params = typeof(_args).parameters
-                    str = join(params .|> show_pattern_type, ", ")
-                    @error "Missing pattern $f_name($str)"
-                end
-                rethrow(e)
-            end
-        end
-    end
-end
-
-"""
-    @pattern function definition
-
-Create a function with symbols as pattern in the signature.
+Create a function that can be called by `@pattern`.
 
 # Examples
 ```jldoctest
@@ -83,28 +35,73 @@ julia> @pattern function f(:one)
        end
 f (generic function with 2 methods)
 
-julia> @pattern f(:two) = 2
-f (generic function with 3 methods)
-
-julia> @pattern function f(:double, x::Number)::Number
-           return 2x
+julia> @pattern function f(:double, x::Number; plus::Number = 0)::Number
+           return 2x + plus
        end
 f (generic function with 4 methods)
+```
 
-julia> f(:two)
-2
+---
+    @pattern <code>
 
-julia> f(:double, 123)
-246
+Resolve pattern.
+
+# Examples
+```jldoctest
+julia> @pattern f(:one)
+1
+
+julia> @pattern f(:double, 123; plus = 1)
+247
+
+julia> @pattern @assert isone(f(:one))
+
+julia> @pattern f(:one) + f(:one) == f(:double, 1)
 ```
 """
-macro pattern(f)
-    def::Dict = splitdef(f)
-    def[:args] = map(do_replace, def[:args])
-    return esc(combinedef(def))
+macro pattern(expr)
+    if is_definition(expr) # Pattern in function definition
+        def::Dict{Symbol, Any} = splitdef(expr)
+
+        # Replace tokens in signature
+        def[:args] = map(replace_token(T -> :(::$T)), def[:args])
+
+        expr = combinedef(def)
+
+        return esc(expr)
+    else
+        expr = postwalk(replace_token(T -> :($T())), expr)
+        return esc(expr)
+    end
 end
 
-do_replace(arg::QuoteNode) = :(::Val{$arg})
-do_replace(arg) = arg
+"""
+Abbreviation for `@pattern`.
+"""
+macro p(expr...)
+    return :(@pattern $(expr...))
+end
+
+###-----------------------------------------------------------------------------
+### Internals
+###-----------------------------------------------------------------------------
+
+is_definition(expr::Expr)::Bool = isshortdef(expr) || expr.head == :function
+is_definition(_)::Bool = false
+
+is_token(expr::QuoteNode)::Bool = expr.value isa Symbol
+is_token(_)::Bool = false
+
+function replace_token(by::Function, expr)
+    if is_token(expr)
+        return by(Val{expr.value})
+    else
+        return expr
+    end
+end
+
+function replace_token(by::Function)::Fix1
+    return Fix1(replace_token, by)
+end
 
 end # module
